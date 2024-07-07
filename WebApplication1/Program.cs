0d29dc09d700b5def9230ee6b4e03d0928684ab8
Junior Lima <juninhowalker.jl@gmail.com>
2024-07-07 16:11:24 -0300

    Adicionando Serviço "Seedind Service" para popular bancos de Dados

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,22 +2,43 @@ using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql;
 using Microsoft.Extensions.DependencyInjection;
 using WebApplication1.Models;
+using WebApplication1.Data;
+
+
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<WebApplication1Context>(options => {
+
+// Adicionar serviços ao contêiner.
+builder.Services.AddDbContext<WebApplication1Context>(options =>
+{
     var connectionString = builder.Configuration.GetConnectionString("WebApplication1");
     var serverVersion = ServerVersion.AutoDetect(connectionString);
     options.UseMySql(connectionString, serverVersion);
 });
-// Add services to the container.
+
+// Registrar o serviço de seeding
+builder.Services.AddScoped<SeedingService>();
+
+// Adicionar controladores com views
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Chamar o serviço de seeding
+SeedDatabase(app);
+
+void SeedDatabase(WebApplication app)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var seedingService = scope.ServiceProvider.GetRequiredService<SeedingService>();
+        seedingService.Seed();
+    }
+}
+
+// Configurar o pipeline de requisições HTTP.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
